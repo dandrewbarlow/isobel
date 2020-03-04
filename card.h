1d93799dab6d46a07a0817991f977d08f24d0092
Andrew Barlow <dandrewbarlow@gmail.com>
2020-03-04 15:27:35 -0700

    header files

---
+++ card.h
@@ -0,0 +1,34 @@
+// #ifnddef CARD_H
+// #def CARD_H
+
+// #include <string>
+
+class card {
+private:
+
+	// indicate the card suit bc values are only 1-4
+		// 0 : major arcana
+		// 1 : pentacles
+		// 2 : cups
+		// 3 : swords
+		// 4 : wands
+	int suit;
+
+	// card number
+	int number;
+
+	// indicate major / minor arcana
+	bool major;
+
+
+	// access functions
+public:
+	card();
+  std::string getSuit();
+  int getNumber();
+  bool getMajor();
+  std::string getName();
+
+};
+
+// #endif
