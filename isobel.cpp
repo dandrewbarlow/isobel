3414b00a65861d69f112d585562ba6b132509721
Andrew Barlow <dandrewbarlow@gmail.com>
2020-06-02 03:31:05 -0600

    signal handling, aesthetic changes

--- isobel.cpp
+++ isobel.cpp
@@ -12,20 +12,38 @@
 #include <random>
 // Time
 #include <time.h>
+// Signal handling
+#include <signal.h>
 
 // card class header
 #include "card.h"
 
 using namespace std;
 
+// SIGNAL HANDLERS////////////////////////////////////////
+void signalHandler(int signum) {
+  if (signum == SIGINT) {
+    cout << "\nInterrupt Signal Recieved\nHave a nice day\n";
+    exit(0);
+  }
+}
 
 // HELPER FUNCTIONS///////////////////////////////////////
 
 // print a 30 char line of unicode block elements
-void linePrint() {
-  for (int i = 0; i < 30; i++) {
-    cout << "\u2500";
+void linePrint(bool position) {
+  //0 = top; 1 = bottom
+  if (position){
+    for (int i = 0; i < 30; i++) {
+      cout << "\u2594";
+    }
   }
+  else {
+    for (int i = 0; i < 30; i++) {
+      cout << "\u2581";
+    }
+  }
+
   cout << endl;
 }
 
@@ -36,24 +54,25 @@ void inputMarker() {
 
 // Print main menu
 void menu() {
-  linePrint();
+  linePrint(0);
   // \u2503 is a vertical line in unicode
   cout << "0 " << "\u2503" << " Exit Application" << endl;
   cout << "1 " << "\u2503" << " Draw a card" << endl;
   cout << "2 " << "\u2503" << " Draw multiple cards" << endl;
   cout << "3 " << "\u2503" << " Draw a spread" << endl;
-  linePrint();
+  linePrint(1);
 }
 
 int spreadMenu() {
-  linePrint();
+  linePrint(0);
   //only one option for now, but
   cout << "0 " << "\u2503" << " Cancel" << endl;
 	cout << "1 " << "\u2503" << " Celtic Cross" << endl;
-  linePrint();
+  linePrint(1);
   inputMarker();
   int input;
   cin >> input;
+  cout << endl;
   if (input != 0 && input != 1) {
     cout << "invalid input, please try again" << endl;
     // recursive gang represent
@@ -71,6 +90,8 @@ int handHolder() {
 
   int input;
   cin >> input;
+  // just for aesthetic reasons
+  cout << endl;
 
   if (cin.fail()){
 	cout << "Not a valid input" << endl;
@@ -119,7 +140,7 @@ void celticSpread() {
   position[8] = "Hopes and Fears",
   position[9] = "Outcome";
   card myCard[10];
-  linePrint();
+  linePrint(1);
   for (int i = 0; i < 10; i++) {
     cout << position[i] << ":" << endl;
     cardInfo(myCard[i]);
@@ -132,13 +153,14 @@ int ui() {
   inputMarker();
 	int input;
 	cin >> input;
+  cout <<endl;
 
   switch (input) {
 
     //draw a card
     case 1:
     {
-      linePrint();
+      linePrint(1);
       card myCard;
       cardInfo(myCard);
       break;
@@ -148,7 +170,7 @@ int ui() {
     case 2:
 	    {
         int number = handHolder();
-        linePrint();
+        linePrint(1);
         card myCard[number];
         for (int i = 0; i < number; i++) {
           cardInfo(myCard[i]);
@@ -181,7 +203,8 @@ int ui() {
 void init() {
 	// seed random number generator
 	srand(time(NULL));
-
+  // register signals
+  signal(SIGINT, signalHandler);
 }
 
 
